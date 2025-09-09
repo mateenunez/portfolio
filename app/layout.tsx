4f8eeaa5cd429733a770f983aa84c4da4fbbcf7c
mateenunez <mateenunez@gmail.com>
2025-09-09 11:33:58 -0300

    added google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,33 @@
-import "@/app/globals.css"
-import { Inter } from "next/font/google"
-import type React from "react"
+import "@/app/globals.css";
+import { Inter } from "next/font/google";
+import type React from "react";
 
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-inter",
-})
+});
 
 export const metadata = {
   title: "Mateo Nuñez",
-  description: "Hola, soy Mateo Nuñez, actualmente estudiante de Sistemas. Podés ver mi experiencia y trabajo en este portafolio.",
-}
+  description:
+    "Hola, soy Mateo Nuñez, actualmente estudiante de Sistemas. Podés ver mi experiencia y trabajo en este portafolio.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="es" className={inter.variable}>
+      <head>
+        <meta
+          name="google-site-verification"
+          content="eF8UbK8ghA29zsWnzurWzjJgXzmLeucRd59QUDpdHTE"
+        />
+      </head>
       <body className="font-sans">{children}</body>
     </html>
-  )
+  );
 }
